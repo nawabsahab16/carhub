aa390e3cd7ce42f616b5f662d446e8a029fdfccc
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-06-10 11:25:50 +0530

    updated the hero section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+const CustomButton = () => {
+  return (
+    <button 
+      disabled={false}
+      type={"button"}
+      className={`custom-btn`}
+      onClick={() => {}}
+      >
+
+    </button>
+
+  )
+}
+
+export default CustomButton
